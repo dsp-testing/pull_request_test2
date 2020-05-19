8f12970484f3fc1891dfc2ca64ea86602f017ded
Marco Gario <marcogario@github.com>
2020-05-19 12:04:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-
+        // Changes from a fork
         char *copy;
         copy = (char *)malloc(strlen(argv[0]));
         strcpy(copy, argv[0]);
