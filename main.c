02d9640a14f0e9819c49e1cce47f64c4a888ef8e
Marco Gario <marcogario@github.com>
2020-05-19 12:03:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 int main(int argc, char **argv) {
-    int x = 0;
-    x = x;
-    return 0;
+        char *copy;
+        copy = (char *)malloc(strlen(argv[0]));
+        strcpy(copy, argv[0]);
+        return copy;
 }
-
